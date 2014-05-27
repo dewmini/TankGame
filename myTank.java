d3f669ffa8da049d906f5be89478d2ce6bbca550
dewmini <kyasima@yahoo.com>
2014-05-27 23:34:14 +0530

    Create myTank.java

---
+++ myTank.java
@@ -0,0 +1,16 @@
+package UI;
+
+public class MyTank {
+    private int myDirection;
+
+    public int getMyDirection() {
+        return myDirection;
+    }
+
+    public void setMyDirection(int myDirection) {
+        this.myDirection = myDirection;
+    }
+    
+    
+}
+
